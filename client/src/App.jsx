cb5545eaec974ea909f486606cd01e4a1c1e430a
AkshJ2004 <imperialaksh.2004@gmail.com>
2025-11-16 23:05:22 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import Sidebar from './components/Sidebar.jsx'
+import Credits from './pages/Credits.jsx'
+import Community from './pages/Community.jsx'
+import { Route,Routes } from 'react-router-dom'
+import ChatBox from './components/ChatBox.jsx'
+
+
+const App = () => {
+  return (
+    <>
+    <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000]
+    dark:text-white'>
+      <div className='flex h-screen w-screen'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<ChatBox />} />
+          <Route path='/credits' element={<Credits />} />
+          <Route path='/community' element={<Community />} />
+        </Routes>
+      </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
